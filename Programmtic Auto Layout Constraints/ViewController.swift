8c54586216b75fa83896d83f23804f87cd45ba72
Brian Robinson <br7pc@mail.missouri.edu>
2019-09-13 15:36:56 -0500

    Initial Commit

---
+++ Programmtic Auto Layout Constraints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Programmtic Auto Layout Constraints
+//
+//  Created by Brian Robinson on 9/13/19.
+//  Copyright © 2019 Brian Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
