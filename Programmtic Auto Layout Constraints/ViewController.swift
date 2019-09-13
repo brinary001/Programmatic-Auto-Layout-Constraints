dc3b8cbcd7ff08da3326a1932c15a1bda4927a46
Brian Robinson <br7pc@mail.missouri.edu>
2019-09-13 16:26:08 -0500

    Committing...

--- Programmtic Auto Layout Constraints/ViewController.swift
+++ Programmtic Auto Layout Constraints/ViewController.swift
@@ -12,8 +12,60 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        createLayout()
         // Do any additional setup after loading the view.
     }
+    
+    func createLayout() {
+        let superview = self.view
+        let myLabel = UILabel()
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        myLabel.text = "My Label"
+        
+        
+        
+        let myButton = UIButton()
+        
+        myButton.setTitle("My Button", for: UIControl.State.normal)
+        myButton.backgroundColor = UIColor.blue
+        myButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        superview?.addSubview(myLabel)
+        superview?.addSubview(myButton)
+        
+        func createLayout() {
+            let superview = self.view
+            
+            let myLabel = UILabel()
+            myLabel.translatesAutoresizingMaskIntoConstraints = false
+            myLabel.text = "My Label"
+            
+            let myButton = UIButton()
+            
+            myButton.setTitle("My Button", for: UIControl.State.normal)
+            myButton.backgroundColor = UIColor.blue
+            myButton.translatesAutoresizingMaskIntoConstraints = false
+            
+            superview?.addSubview(myLabel)
+            superview?.addSubview(myButton)
+        }
+    }
+    
+    
+//    @IBOutlet var view1: UIView!
+//    @IBOutlet var view2: UIView!
+//
+//
+//    var myConstraint =
+//        NSLayoutConstraint(item: view1,
+//            attribute: NSLayoutConstraint.Attribute.bottom,
+//            relatedBy: NSLayoutConstraint.Relation.equal,
+//            toItem: view2,
+//            attribute: NSLayoutConstraint.Attribute.bottom,
+//            multiplier: 1.0,
+//            constant: -20)
+    
+    
 
 
 }
